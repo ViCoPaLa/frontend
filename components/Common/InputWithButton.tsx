d315f09a3e2303b0bb9fac559eea419831ce94b6
r4bb1t <r4bb1t.github@gmail.com>
2023-09-03 01:19:04 +0900

    feat: add keyboard enter event to inputwithbutton

--- components/Common/InputWithButton.tsx
+++ components/Common/InputWithButton.tsx
@@ -2,18 +2,31 @@ import { ReactNode } from "react";
 
 export default function InputWithButton({
   button,
+  disabled = false,
   onClick,
 }: {
   button: ReactNode;
+  disabled?: boolean;
   onClick: Function;
 }) {
   return (
-    <div className="flex gap-2 w-full border border-gray-500 rounded-lg p-2">
+    <div className="flex gap-2 w-full border border-gray-500 rounded-lg p-2 bg-background">
       <div className="w-full px-2 flex items-center">
-        <input className="w-full bg-transparent focus:outline-none" />
+        <textarea
+          className="w-full bg-transparent focus:outline-none resize-none"
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              //@ts-ignore
+              if (e.isComposing) return;
+              e.preventDefault();
+              onClick();
+            }
+          }}
+        />
       </div>
       <button
-        className="bg-black px-4 py-2 rounded-lg"
+        disabled={disabled}
+        className="bg-[#0D622F] px-4 py-3 rounded-lg hover:bg-[#123821] disabled:bg-gray-500 transition-colors"
         onClick={() => onClick()}
       >
         {button}
--- pages/scene/[id].tsx
+++ pages/scene/[id].tsx
@@ -1,4 +1,5 @@
 import { useLayout } from "@/Contexts/useLayoutContext";
+import InputWithButton from "@/components/Common/InputWithButton";
 import Hint from "@/components/Hint";
 import Icons from "@/components/Icons";
 import { useEffect } from "react";
@@ -17,11 +18,24 @@ export default function Scene() {
 
   return (
     <>
-      <div className="w-full h-16 flex items-center justify-center gap-2 text-2xl">
-        <Icons.Location className="w-8 h-8 stroke-black" />
-        집현전
-      </div>
-      <main className="w-full h-screen flex flex-col">asdf</main>
+      <main className="relative w-full h-screen flex flex-col ">
+        <div className="absolute top-0 w-full h-16 flex items-center justify-center gap-2 text-2xl">
+          <Icons.Location className="w-8 h-8 stroke-black" />
+          집현전
+        </div>
+        <div className="w-full h-full overflow-y-auto pt-16 pb-24">
+          <div className="h-[150vh] bg-gradient-to-b from-red-300 to-red-500">
+            asdfasfd
+          </div>
+        </div>
+        <div className="absolute w-full bottom-0 pb-8 px-4">
+          <InputWithButton
+            disabled
+            button={<Icons.Send className="w-6 h-6 stroke-white" />}
+            onClick={() => {}}
+          />
+        </div>
+      </main>
       <Hint
         text={`<h1>한글의 제자 원리</h1>\n<strong>1. 상형의 원리</strong>\n일정한 형상을 본따서 모양을 만듦.\n자음의 기본자(‘ㄱ’, ‘ㄴ’, ‘ㅁ’, ‘ㅅ’, ‘ㅇ’)는\n발음 기관의 모양을 본떠서 만들었어요.\n\n모음의 기본자(· , ㅡ, ㅣ)는\n천지인(하늘, 땅, 사람)의 모양을 본떠서 만들었어요.\n\n<strong>2. 가획의 원리</strong>\n\n‘ㅋ’, ‘ㅌ’ 등의 자음은 기본자에서\n획을 더해 더 센 소리를 표현했어요.\n\n<strong>3. 합용의 원리</strong>\n낱낱의 글자를 다시 합하여 사용함.\n\n‘ㄲ’, ‘ㄸ’, ‘ㅃ’ 등의 같은 자음은\n옆으로 나란히 합쳐서 만들었어요.`}
       />
