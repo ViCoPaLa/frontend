aea4c34ee8637f0dd50397f7940d7dc5b410f3e7
r4bb1t <r4bb1t.github@gmail.com>
2023-09-03 03:54:33 +0900

    fix: 디자인 맞춤

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -4,10 +4,10 @@ import Icons from "../Icons";
 export default function Intro({ type }: { type: "sejong" }) {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-12 xl:bg-background bg-primary pb-12 xl:pb-0">
-      <img src="/logo.png" className="w-2/3" />
+      <img src="/logo.png" className="xl:hidden w-2/3" />
       <img
         src="/stories/sejong.png"
-        className="w-56 h-56 rounded-full border-[16px] border-white xl:border-transparent xl:intro-profile"
+        className="w-56 h-56 rounded-full border-[16px] border-background xl:border-transparent xl:intro-profile"
       />
       <Link
         href="/scene/1"
