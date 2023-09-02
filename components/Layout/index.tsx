a4e735b7e999a69132e69a96a23ef421541afee5
r4bb1t <r4bb1t.github@gmail.com>
2023-09-03 03:44:33 +0900

    fix: background 있을 때만 fade-in되게

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -64,7 +64,7 @@ export default function Layout({
           ])}
         >
           <AnimatePresence>
-            {isBackgroundOpened && (
+            {isBackgroundOpened && background && (
               <>
                 <motion.div
                   initial={{ opacity: 1 }}
